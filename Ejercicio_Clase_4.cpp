727198b15751292144d974a7cc849355892dd459
danivivi <30662920+danivivi@users.noreply.github.com>
2017-11-01 22:22:10 -0500

    Update Ejercicio_Clase_4.cpp

--- Ejercicio_Clase_4.cpp
+++ Ejercicio_Clase_4.cpp
@@ -15,26 +15,28 @@ struct persona{
 	int edad;
 	fecha nacimiento;
 };
-int personitas[100],tam;
-void datos(persona v[],int tam,int personitas[]);
-int pasar_dias(int dia,int mes,int anio,int dia_fin,int mes_fin,int anio_fin);
-void imprimir(int personitas[],int tam);
+int personitas[100];
+void datos(persona v[],int &tam,int personitas[]);
+void pasar_dias(int dia,int mes,int anio,int dia_fin,int mes_fin,int anio_fin,int &numdias_inicio,int &numdias_fin);
+void imprimir(persona v[],int personitas[],int tam,int numdias_inicio,int numdias_fin);
 
 int main()
-{
+{	
+	int tam=0;
 	persona v[100];
-	datos(v,tam,personitas);
+	datos(v,tam,personitas);	
 	return 0;
 }
 
-void datos(persona v[],int tam,int personitas[])
+void datos(persona v[],int &tam,int personitas[])
 {
-	int dia,mes,anio,anio_actual,numdias_inicio;
-	int dia_fin,mes_fin,anio_fin,numdias_fin,opcion;
+	int numdias_inicio=0,numdias_fin=0;
+	int dia,mes,anio,anio_actual;
+	int dia_fin,mes_fin,anio_fin,opcion;
 	
 	printf("Ingrese cuantas personas va a registrar:\n");
 	scanf("%d",&tam);
-	printf("Ingrese Año Actual: ");
+	printf("Ingrese AÃ±o Actual: ");
 	scanf("%d",&anio_actual);
 	for(int i=0;i<tam;i++)
 	{
@@ -44,58 +46,56 @@ void datos(persona v[],int tam,int personitas[])
 		printf("%i.Apellido:",i+1);
 		fflush(stdin);
 		fgets(v[i].apellido,30,stdin);
-		printf("%i.Genero,Ingrese una opcion: 1.Femenino ó 2.Masculino\n",i+1);
+		printf("%i.Genero,Ingrese una opcion: 1.Femenino Ã³ 2.Masculino\n",i+1);
 		scanf("%d",&opcion);
 		switch(opcion)
 		{
 		case 1:
 			v[i].sexo=false;
+			break;
 		case 2:
 			v[i].sexo=true;
+			break;
 		}
 		printf("%i.Fecha de Nacimiento:\ndia:",i+1);
 		scanf("%d",&v[i].nacimiento.dia);
 		printf("Mes:");
 		scanf("%d",&v[i].nacimiento.mes);
-		printf("Año:");
+		printf("AÃ±o:");
 		scanf("%d",&v[i].nacimiento.anio);		
 		v[i].edad=anio_actual-v[i].nacimiento.anio;
 		personitas[i]=v[i].nacimiento.dia+(v[i].nacimiento.mes*30)+(v[i].nacimiento.anio*365);
 	}
-	printf("Ingrese Fecha entre las que desea encontrar:\n");
+	printf("Ingrese rango de fecha a buscar:\n");
 	printf("Fecha Inicial: Dia");
 	scanf("%d",&dia);
 	printf("Mes:");
 	scanf("%d",&mes);
-	printf("Año:");
+	printf("AÃ±o:");
 	scanf("%d",&anio);
 	printf("Fecha Final: Dia");
 	scanf("%d",&dia_fin);
 	printf("Mes:");
 	scanf("%d",&mes_fin);
-	printf("Año:");
+	printf("AÃ±o:");
 	scanf("%d",&anio_fin);	
-	pasar_dias(dia,mes,anio,dia_fin,mes_fin,anio_fin);
-	imprimir(personitas,tam);
+	pasar_dias(dia,mes,anio,dia_fin,mes_fin,anio_fin,numdias_inicio,numdias_fin);
+	imprimir(v,personitas,tam,numdias_inicio,numdias_fin);
 }
 
-int pasar_dias(int dia,int mes,int anio,int dia_fin,int mes_fin,int anio_fin)
-{
-	int numdias_inicio,numdias_fin;
-	
+void pasar_dias(int dia,int mes,int anio,int dia_fin,int mes_fin,int anio_fin,int &numdias_inicio,int &numdias_fin)
+{	 
 	numdias_inicio=dia+(mes*30)+(anio*365);
 	numdias_fin=dia_fin+(mes_fin*30)+(anio_fin*365);
-	
-	return numdias_inicio,numdias_fin;
 }
 
-void imprimir(int personitas[],int tam)
+void imprimir(persona v[],int personitas[],int tam,int numdias_inicio,int numdias_fin)
 {
-	int numdias_inicio,numdias_fin;
-	persona v[100];
+	
+	
 	for(int i=0;i<tam;i++)
 	{
-		if (numdias_inicio>=personitas[i] && numdias_fin<=personitas[i])
+		if (numdias_inicio<=personitas[i] && numdias_fin>=personitas[i])
 		{
 			if(v[i].sexo==true)
 			{
@@ -104,3 +104,4 @@ void imprimir(int personitas[],int tam)
 		}		
 	}
 }
+
